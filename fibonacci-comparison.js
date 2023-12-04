ec9fbc124a832bfc03795d112afadb66301fec90
swansestudio <swansestudio@yahoo.com>
2023-12-04 23:44:28 +0300

    feat: add javascript fibonacci loop algorithm

--- fibonacci-comparison.js
+++ fibonacci-comparison.js
@@ -1,11 +1,11 @@
-const number = 40;
+console.clear();
+
+const number = 25;
 
 /* Fibonacci recurse */
 const fibonacci = function fib(n) {
   return n < 2 ? n : fib(n - 1) + fib(n - 2);
 };
-
-
 
 /* Fibonacci recurse with cache */
 const cache = {};
@@ -22,6 +22,18 @@ function fibonacciCached(n) {
   return cache[n];
 }
 
+/* Fibonacci Non-recursive (Loop) */
+
+function fibonacciLoop(n) {
+  let a = 0, b = 1, tmp;
+  for (let i = 0; i < n; i++) {
+    tmp = a;
+    a = b;
+    b = tmp + b;
+  }
+  return a;
+}
+
 /* Driver and time measurement code */
 console.time("fibonacci Recurse");
 console.log(`Fibonacci of ${number} = ${fibonacci(number)}`);
@@ -31,4 +43,9 @@ console.log();
 console.time("fibonacci CACHED");
 console.log(`Fibonacci of ${number} = ${fibonacciCached(number)}`);
 console.timeEnd("fibonacci CACHED");
+console.log();
+
+console.time("fibonacci LOOP");
+console.log(`Fibonacci of ${number} = ${fibonacciLoop(number)}`);
+console.timeEnd("fibonacci LOOP");
 console.log();
